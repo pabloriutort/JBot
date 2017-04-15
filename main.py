2c51600c1ab6bb9a8681d9a97bb9249b63ca5d32
Pablo <pablo.riutort@gmail.com>
2017-04-15 19:29:50 +0200

    Add debug and logging

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from JBot import JBot
 import commands as co
+import logging
+from os import environ
 
 
+# set logging
+logging.basicConfig(
+    level=logging.WARNING,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+if 'DEBUG' in environ and environ['DEBUG'] == '1':
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+
+
+# initialize JB
 jb = JBot()
 
 jb.add_command('hello', co.hello)
 jb.add_command('reply', co.reply_text)
 jb.add_command('send', co.send_text)
+
 jb.listen()
 
